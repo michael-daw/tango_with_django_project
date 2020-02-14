b5cf5ffb5670dc733792af01cda8abd7c40777c2
Michael Daw <michael7daw@gmail.com>
2020-02-14 11:36:42 +0000

    chapter 9 complete

--- rango/models.py
+++ rango/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.template.defaultfilters import slugify
-#from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     
@@ -34,10 +34,10 @@ class Page(models.Model):
     def __str__(self):
         return self.title
 
-#class userProfile(models.Model):
-#    user = models.OneToOneField(User, on_delete=models.CASCADE)
-#    website = models.URLField(blank=True)
-#    picture = models.ImageField(upload_to='profile_images', blank=True)
-#
-#    def __str__(self):
-#        return self.user.username
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+    
+    def __str__(self):
+        return self.user.username
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -21,6 +21,8 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
+LOGIN_URL = 'rango:login'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -96,7 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', #'OPTIONS': { 'min_length' : 6, }
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', 'OPTIONS': { 'min_length' : 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -106,12 +108,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-#PASSWORD_HASHERS = [
-#    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-#    'django.contrib.auth.hashers.BCryptPasswordHasher',
-#    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-#    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
-#]
+#using hotspot, couldn't download bcrypt
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+]
 
 
 # Internationalization
